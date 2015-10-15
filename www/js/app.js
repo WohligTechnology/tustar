dcea56977eb9e09c97eb477480a97d21b0cca92d
sohan1 <shn619@gmail.com>
2015-10-15 15:54:57 +0530

    chnages

--- www/js/app.js
+++ www/js/app.js
@@ -8,8 +8,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function ($ionicPlatform) {
 	$ionicPlatform.ready(function () {
+		if (window && window.plugins && window.plugins.socialsharing && window.plugins.socialsharing.share) {
+			socialShare = window.plugins.socialsharing.share;
+		}
 		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
+		}
 		if (window.cordova && window.cordova.plugins.Keyboard) {
 			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 			cordova.plugins.Keyboard.disableScroll(true);
@@ -89,4 +96,41 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 	// if none of the above states are matched, use this as the fallback
 	$urlRouterProvider.otherwise('/app/home');
+})
+
+.directive('fbPost', function ($document) {
+	return {
+		restrict: 'EA',
+		replace: false,
+		link: function ($scope, element, attr) {
+			(function (d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s);
+				js.id = id;
+				js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=1652034465042425";
+				fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+		}
+	}
+})
+
+
+.directive('tweetBox', function ($document) {
+	return {
+		restrict: 'EA',
+		replace: false,
+		link: function ($scope, element, attr) {
+			! function (d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0],
+					p = /^http:/.test(d.location) ? 'http' : 'https';
+				if (!d.getElementById(id)) {
+					js = d.createElement(s);
+					js.id = id;
+					js.src = p + "://platform.twitter.com/widgets.js";
+					fjs.parentNode.insertBefore(js, fjs);
+				}
+			}(document, "script", "twitter-wjs");
+		}
+	}
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,14 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova'])
 
 .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {})
 
 .controller('LoginCtrl', function ($scope) {})
 
-.controller('HomeCtrl', function ($scope, $stateParams) {})
+.controller('HomeCtrl', function ($scope, $stateParams,$cordovaSocialSharing) {
+	$scope.share = function (item) {
+		window.plugins.socialsharing.share('Checkout "' + item.title + '" starting on ' + data.startdate + ', ' + data.starttime, null, data.image + 'At ' + item.venue);
+	}
+})
 
 .controller('NotificationCtrl', function ($scope, $stateParams) {})
 
